b225546a0b6b49720a89290d52bc1832d98defd8
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-05-28 14:57:17 +0200

    add function remove hamburger menu after click

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ toggle.addEventListener("click", () => {
   navMenu.classList.toggle("show-menu");
   toggle.classList.toggle("toggleX");
 });
+
+// REMOVING HAMBURGER MENU AFTER CLICK
+const allLinks = document.querySelectorAll(".nav__link");
+
+const linkAction = function () {
+  const navMenu = document.querySelector(".nav__menu--js");
+  navMenu.classList.remove("show-menu");
+  toggle.classList.remove("toggleX");
+};
+allLinks.forEach((n) => n.addEventListener("click", linkAction));
